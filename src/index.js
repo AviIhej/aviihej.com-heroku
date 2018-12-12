a6b5b0452c1048a829b7e1f575bac2ac73d8ce3c
Avi Ihej <aviihej@gmail.com>
2018-12-11 20:20:22 -0500

    add https redirect

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import HttpsRedirect from 'react-https-redirect';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( <HttpsRedirect><App /></HttpsRedirect>, document.getElementById('root'));
 
 registerServiceWorker();
